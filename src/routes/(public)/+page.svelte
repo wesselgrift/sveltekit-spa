f232ffbbcc5071c54f5b0a84b5aa013786642b04
Wessel Grift <wesselgrift@users.noreply.github.com>
2025-12-25 14:04:16 +0100

    Fix comment

--- src/routes/(public)/+page.svelte
+++ src/routes/(public)/+page.svelte
@@ -2,17 +2,12 @@
 	/**
 	 * Public Homepage
 	 * 
-	 * Public homepage accessible at / (root path).
+	 * Public page accessible at '/' (no authentication required).
 	 * 
-	 * Will contain:
-	 * - Welcome message/content
-	 * - Navigation links to login/signup using ShadCN Button components
-	 * - No authentication required
 	 */
 </script>
 
 <div class="p-4 space-y-2">
 	<h1>Welcome to Svelte Spa, a sveltekit SPA boilerplate</h1>
-	<!-- TODO: Add navigation links to login/signup using ShadCN Button components -->
 </div>
 
