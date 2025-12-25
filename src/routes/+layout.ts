49d8d0943b6c05416425b36c8c44d580afcb9193
Wessel Grift <wesselgrift@users.noreply.github.com>
2025-12-25 11:50:50 +0100

    configure SPA mode and add ShadCN Svelte

---
+++ src/routes/+layout.ts
@@ -0,0 +1,2 @@
+// Set all pages to prerender for SPA mode
+export const prerender = true;
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,7 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<script>
+    import { Button } from "$lib/components/ui/button";
+</script>
+
+<h1>Welcome to Svelte Spa, a sveltekit SPA boilerplate</h1>
+
+<Button>Button</Button>
