e9ac02930ae53cec232da2b3b62503553c404cba
Wessel Grift <wesselgrift@users.noreply.github.com>
2025-12-26 23:02:07 +0100

    sending simple template with resend

--- src/lib/server/email/sendEmail.server.ts
+++ src/lib/server/email/sendEmail.server.ts
@@ -1,5 +1,6 @@
 import { Resend } from 'resend'
 import { RESEND_API_KEY } from '$env/static/private';
+import template from './templates/verifyEmail.html?raw';
 
 const resend = new Resend(RESEND_API_KEY);
 
@@ -9,11 +10,15 @@ export type SendEmailInput = {
 	text: string;
 };
 
+const htmlString = template
+
+console.log(htmlString);
+
 export async function sendEmail(input: SendEmailInput): Promise<void> {
     await resend.emails.send({
         from: 'Sveltekit SPA <test@resend.dev>',
         to: input.to,
         subject: input.subject,
-        text: input.text
+        html: template
     });
 } 
