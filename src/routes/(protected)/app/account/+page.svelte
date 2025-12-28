03925ad3e8806d45eb09fef2890c910985273d52
Wessel Grift <wesselgrift@users.noreply.github.com>
2025-12-28 20:00:00 +0100

    styling fix

--- src/routes/(protected)/app/account/+page.svelte
+++ src/routes/(protected)/app/account/+page.svelte
@@ -6,7 +6,7 @@
     import { AccountSettings } from '$lib/components/account'
 </script>
 
-<div class="container mx-auto p-6 max-w-2xl">
+<div class="container mx-auto p-6 max-w-2xl animate-fade-in-zoom">
 	<div class="flex flex-col gap-2 mb-5">
 		<h1 class="text-2xl font-semibold">Account</h1>
         <p class="text-sm text-muted-foreground">This is a simple account component which you can use anywhere in your app.</p>
