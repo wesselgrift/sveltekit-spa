5925fb8211d4cc2b8eb87a5ab4dd072318d6140d
Wessel Grift <wesselgrift@users.noreply.github.com>
2025-12-28 13:28:43 +0100

    prep interactivity for name change

--- src/lib/components/account/name-section.svelte
+++ src/lib/components/account/name-section.svelte
@@ -2,7 +2,11 @@
     import { authState } from '$lib/auth/state.svelte';
     import { splitDisplayName } from '$lib/helpers/name-helpers';
     import { Button } from '$lib/components/ui/button'
+    import { Input } from '$lib/components/ui/input'
+    import { Label } from '$lib/components/ui/label'
     import { IdCardLanyard } from '@lucide/svelte'
+
+    let changeName = $state(false);
 
     let nameParts = $derived(
         authState.user?.displayName ? 
@@ -12,11 +16,30 @@
     );
 </script>
 
-<div class="flex flex-row gap-4 p-4 w-full border-b">
-    <IdCardLanyard class="shrink-0" strokeWidth={1.5} />
-    <div class="flex flex-col gap-1 text-sm">
-        <p class="font-medium">Name</p>
-        <p class="text-muted-foreground">{nameParts.firstName} {nameParts.lastName}</p>
+<div class="flex flex-col border-b">
+    <div class="flex flex-row gap-4 p-4 w-full">
+        <IdCardLanyard class="shrink-0" strokeWidth={1.5} />
+        <div class="flex flex-col gap-1 text-sm">
+            <p class="font-medium">Name</p>
+            <p class="text-muted-foreground">{nameParts.firstName} {nameParts.lastName}</p>
+        </div>
+        <Button onclick={() => changeName = true} variant="outline" size="sm" class="ml-auto" disabled={changeName}>Change name</Button>
     </div>
-    <Button variant="outline" size="sm" class="ml-auto">Change name</Button>
+
+    {#if changeName}
+        <div class="flex flex-col p-4 pl-14 max-w-sm gap-5">
+            <div class="flex flex-col gap-2.5">
+                <Label>First Name</Label>
+                <Input value={nameParts.firstName}/>
+            </div>
+            <div class="flex flex-col gap-2.5">
+                <Label>Last Name</Label>
+                <Input value={nameParts.lastName}/>
+            </div>
+            <div class="flex flex-row gap-2 mb-4">
+                <Button variant="default" size="sm">Save</Button>
+                <Button onclick={() => changeName = false} variant="outline" size="sm">Cancel</Button>
+            </div>
+        </div>
+    {/if}
 </div>
